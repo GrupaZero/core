81dceff90aee181ad01b071e6e4a6f6337f36c88
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2017-12-28 13:38:15 +0100

    Created UserViewModel

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,3 +1,4 @@
+<?php /* @var $user \Gzero\Core\ViewModels\UserViewModel */ ?>
 @extends('gzero-core::layouts.master')
 
 @section('title')
@@ -9,14 +10,14 @@
 @component('gzero-core::layouts._contentSection', ['class' => 'col-sm-8'])
     <h1 class="page-header">@lang('gzero-core::user.my_account')</h1>
 
-    <h3>{{ $user->first_name }} {{ $user->lastName }}</h3>
+    <h3>{{ $user->firstName() }} {{ $user->lastName() }}</h3>
 
-    @if(isset($user->hasSocialIntegrations) && strpos($user->email,'social_') !== false)
+    @if($user->hasSocialIntegrations() && strpos($user->email(),'social_') !== false)
         <p>@lang('gzero-core::common.account_connected')</p>
         <p class="text-danger"><i class="fa fa-exclamation-triangle"><!-- icon --></i> @lang('common.email_is_missing')</p>
     @else
         <p>
-            <strong>@choice('gzero-core::common.email', 1):</strong> {{ $user->email }}
+            <strong>@choice('gzero-core::common.email', 1):</strong> {{ $user->email() }}
             <small class="help-block">@lang('gzero-core::common.email_is_hidden')</small>
         </p>
     @endif

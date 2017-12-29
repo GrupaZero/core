bbab5958c5392a5c5714f4c8aa8fdd07c2a42150
Przemysław Skierniewski <skierniewski.przemyslaw@gmail.com>
2017-12-29 12:23:10 +0100

    Fixed blocks in regions partials (#25)

--- resources/views/layouts/_sidebarRightSection.blade.php
+++ resources/views/layouts/_sidebarRightSection.blade.php
@@ -3,7 +3,7 @@
         <aside id="sidebarRight" class="{{ isset($class) ? $class : 'col-sm-4' }}">
             @if(!empty($blocks))
                 @foreach($blocks as $index => $block)
-                    {!! $block->getView() !!}
+                    {!! $block->view() !!}
                 @endforeach
             @endif
 

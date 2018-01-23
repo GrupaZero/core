1c12aace7e0e03763f3bec9b6eb1512b0384ed2e
Michał Strzelecki <michasstrzelecki@gmail.com>
2018-01-23 14:11:55 +0100

    added fr and de to translation list

--- resources/lang/en/language_names.php
+++ resources/lang/en/language_names.php
@@ -3,6 +3,8 @@
 # modified at 23-01-2018 13:48
 
 return [
+    'de' => 'German',
     'en' => 'English',
+    'fr' => 'French',
     'pl' => 'Polish'
 ];
--- resources/lang/pl/language_names.php
+++ resources/lang/pl/language_names.php
@@ -3,6 +3,8 @@
 # modified at 23-01-2018 13:48
 
 return [
+    'de' => 'Niemiecki',
     'en' => 'Angielski',
+    'fr' => 'Francuski',
     'pl' => 'Polski'
 ];

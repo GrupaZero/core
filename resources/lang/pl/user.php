331d20298594755271f040dea19eca474e6a3b01
Bartero <bartek.krzywania@gmail.com>
2018-02-03 15:19:21 +0100

    Add LanguageCodeIsActive rule + tests

--- resources/lang/pl/user.php
+++ resources/lang/pl/user.php
@@ -3,10 +3,11 @@
 # modified at 13-06-2014 12:53
 
 return [
-    'admin_panel'  => 'Zarządzaj',
-    'edit_account' => 'Edytuj Konto',
-    'get_help'     => 'Pomoc',
-    'my_account'   => 'Moje Konto',
-    'oauth'        => 'OAuth',
-    'user'         => 'Użytkownik',
+    'admin_panel'      => 'Zarządzaj',
+    'edit_account'     => 'Edytuj Konto',
+    'get_help'         => 'Pomoc',
+    'my_account'       => 'Moje Konto',
+    'oauth'            => 'OAuth',
+    'user'             => 'Użytkownik',
+    'invalid_language' => 'Ten język nie istnieje',
 ];

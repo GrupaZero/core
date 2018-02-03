ed63f28d43c29aed51443339a7d927c60947e9ff
Bartero <bartek.krzywania@gmail.com>
2018-02-03 12:43:32 +0100

    work in progress...

--- src/Gzero/Core/Jobs/CreateUser.php
+++ src/Gzero/Core/Jobs/CreateUser.php
@@ -22,27 +22,39 @@ class CreateUser {
     /** @var string */
     protected $lastName;
 
+    /** @var string */
+    protected $languageCode;
+
+    /** @var string */
+    protected $timezone;
+
     /**
      * Create a new job instance.
      *
-     * @param string $email     Email
-     * @param string $password  Password
-     * @param string $name      Name
-     * @param string $firstName First name
-     * @param string $lastName  Last name
+     * @param string $email        Email
+     * @param string $password     Password
+     * @param string $name         Name
+     * @param string $firstName    First name
+     * @param string $lastName     Last name
+     * @param string $languageCode Preferred language
+     * @param string $timezone     User's timezone
      */
     public function __construct(
         string $email,
         string $password,
         ?string $name = null,
         ?string $firstName = null,
-        ?string $lastName = null
+        ?string $lastName = null,
+        string $languageCode = null,
+        string $timezone = null
     ) {
-        $this->email     = $email;
-        $this->password  = $password;
-        $this->name      = $name;
-        $this->firstName = $firstName;
-        $this->lastName  = $lastName;
+        $this->email        = $email;
+        $this->password     = $password;
+        $this->name         = $name;
+        $this->firstName    = $firstName;
+        $this->lastName     = $lastName;
+        $this->languageCode = $languageCode;
+        $this->timezone     = $timezone;
     }
 
     /**
@@ -58,11 +70,13 @@ class CreateUser {
         $user = $this->dbTransaction(function () {
             $user = new User();
             $user->fill([
-                'email'      => $this->email,
-                'password'   => bcrypt($this->password),
-                'name'       => $this->name,
-                'first_name' => $this->firstName ?: null,
-                'last_name'  => $this->lastName ?: null,
+                'email'         => $this->email,
+                'password'      => bcrypt($this->password),
+                'name'          => $this->name,
+                'first_name'    => $this->firstName ?: null,
+                'last_name'     => $this->lastName ?: null,
+                'language_code' => $this->languageCode,
+                'timezone'      => $this->timezone
             ]);
             $user->save();
             event('user.created', [$user]);

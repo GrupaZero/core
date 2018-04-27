850e0e85cf7d6062ae7f5d356cb55242e2fdd986
Bartłomiej Krzywania <bartek.krzywania@gmail.com>
2018-04-27 12:06:23 +0200

    Fix the function tests of the iso8601 validator (#72)

--- tests/functional/Iso8601ValidatorCest.php
+++ tests/functional/Iso8601ValidatorCest.php
@@ -3,13 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
 
 class BunnyController extends Controller {
 
     public function save(Request $request)
     {
         $request->validate(['bunny_date' => "iso8601"]);
+        return "Bunny is happy";
     }
 }
 
@@ -17,20 +17,26 @@ class Iso8601ValidatorCest {
 
     public function _before(FunctionalTester $I)
     {
-        Route::post('/save-bunny', "Core\BunnyController@save");
+        Route::group([
+            'domain'     => 'api.' . config('gzero.domain'),
+            'prefix'     => 'v1',
+        ], function ($router) {
+            $router->post('/save-bunny', "Core\BunnyController@save");
+        });
     }
 
     public function canValidateValidCaseAgainstIso8601(FunctionalTester $I)
     {
-        $I->sendPOST('/save-bunny', ['bunny_date' => '2019-07-04T12:32:12-0300']);
+        $I->sendPOST(apiUrl('save-bunny'), ['bunny_date' => '2019-07-04T12:32:12-0300']);
 
         $I->seeResponseCodeIs(200);
+        $I->seeResponseContains("Bunny is happy");
     }
 
     public function canValidateInvalidCaseAgainstIso8601(FunctionalTester $I)
     {
-        $I->sendPOST('/save-bunny', ['bunny_date' => '2019-07-04T12:32:12']);
+        $I->sendPOST(apiUrl('save-bunny'), ['bunny_date' => '2019-07-04T12:32:12']);
 
-        $I->seeResponseCodeIs(404);
+        $I->seeResponseCodeIs(422);
     }
 }

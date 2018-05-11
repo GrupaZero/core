d178d5353ac60099b08277bedbb68f1a8cc12bd9
Michał Strzelecki <michasstrzelecki@gmail.com>
2018-05-11 11:10:10 +0200

    change class name to becouse of twb update in platform

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 @section('footer')
     <div class="container">
         <div class="clearfix text-muted">
-            <div class="copyrights pull-left">
+            <div class="copyrights float-left">
                 Copyright &copy; {{ config('gzero.domain') }},
                 @lang('gzero-core::common.all_rights_reserved')
             </div>
